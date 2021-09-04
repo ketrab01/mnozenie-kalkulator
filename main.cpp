dc99d8339fd0ac0e79e4c40e473209c7735b52ef
BaPeeK <90092743+ketrab01@users.noreply.github.com>
2021-09-04 18:49:03 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main() 
+{
+    float a = 20;
+	float b = 13;
+	
+
+	std::cout << "Program obliczajacy mnozenie\n";
+	std::cout << "Prosze wprowadzic a \n";	
+	    std::cin >> a;
+		std::cout << "Prosze wprowadzic b \n";
+		std::cin >> b;
+		
+    	
+		
+
+		std::cout << "Pole kwadrata o: \n";
+		std::cout << "a = " << a << "\n";
+		std::cout << "b = " << b << "\n";
+		
+
+		std::cout << (a * b) / 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
